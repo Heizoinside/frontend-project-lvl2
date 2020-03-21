9720d23003e8e2dc71bd2b8c95300d99bc3bd67b
Heizo <heizoinside@gmail.com>
2020-03-21 19:00:55 +0300

    it works with trees

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,18 +5,18 @@ import compare from '../src';
 const formats = ['json', 'yaml', 'ini'];
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-// test.each(formats)('compare', (ext) => {
-//   const expected = fs.readFileSync(getFixturePath('result.txt'), 'utf-8');
-//   const fileBefore = getFixturePath(`before.${ext}`);
-//   const fileAfter = getFixturePath(`after.${ext}`);
-//   const result = compare(fileBefore, fileAfter);
-//   expect(result).toBe(expected);
-// });
+test.each(formats)('compare', (ext) => {
+  const expected = fs.readFileSync(getFixturePath('result.txt'), 'utf-8');
+  const fileBefore = getFixturePath(`before.${ext}`);
+  const fileAfter = getFixturePath(`after.${ext}`);
+  const result = compare(fileBefore, fileAfter);
+  expect(result).toBe(expected);
+});
 test('nested', () => {
   const ext = 'json';
   const fileBefore = getFixturePath(`tree-before.${ext}`);
   const fileAfter = getFixturePath(`tree-after.${ext}`);
   const expected = fs.readFileSync(getFixturePath('tree-result.txt'), 'utf-8');
   const result = compare(fileBefore, fileAfter);
-  expect(result).toBe(result);
+  expect(result).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -53,16 +53,35 @@ const buildAst = (obj1, obj2) => {
   });
 };
 
-const operators = {
-  unchanged: (node) => `  ${node.name}: ${node.before}`,
-  changed: (node) => `- ${node.name}: ${node.before} \n+ ${node.name}:${node.after}`,
-  added: (node) => `+ ${node.name}: ${node.after}`,
-  deleted: (node) => `- ${node.name}: ${node.before}`,
-  nested: (node, func) => ` ${node.name}:\n${func(node.children)}`,
+const makeTab = (tabCount) => ' '.repeat(tabCount);
+
+const stringify = (key, value, tab) => {
+  if (!_.isObject(value)) {
+    return `${makeTab(tab)}${key}: ${value}`;
+  }
+  const innerContent = Object.keys(value).map((el) => (
+    stringify(el, value[el], tab + 5)
+  ));
+  return `${makeTab(tab)}${key}: {\n${innerContent}\n${makeTab(tab + 2)}}`;
+};
+
+const operations = {
+  unchanged: (node, tab) => stringify(`  ${node.name}`, node.before, tab),
+  changed: (node, tab) => {
+    const conentBefore = stringify(`- ${node.name}`, node.before, tab);
+    const contentAfter = stringify(`+ ${node.name}`, node.after, tab);
+    return `${conentBefore}\n${contentAfter}`;
+  },
+  added: (node, tab) => stringify(`+ ${node.name}`, node.after, tab),
+  deleted: (node, tab) => stringify(`- ${node.name}`, node.before, tab),
+  nested: (node, tab, func) => {
+    const childsContent = node.children.map((el) => operations[el.type](el, tab + 3, func)).join('\n');
+    return `${makeTab(tab + 2)}${node.name}: {\n${childsContent}\n${makeTab(tab + 2)}}`;
+  },
 };
 
 const render = (ast) => (
-  ast.map((el) => operators[el.type](el, render)).join('\n')
+  ast.map((el) => operations[el.type](el, 0, render)).join('\n')
 );
 
 export default (filepath1, filepath2) => {
