1ea7d9edc1b59f9d8507140df6ea058204ed1de2
Heizo <heizoinside@gmail.com>
2020-03-22 13:25:12 +0300

    add 4 space indent

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,13 +3,13 @@ import program from 'commander';
 import compareFiles from '..';
 
 program
-  .version('0.0.3')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-f --format <type>', 'output format')
-  .arguments('<firstConfig> <secondConfig>')
-  .action((firstConfig, secondConfig) => {
-    console.log(`${firstConfig} ${secondConfig}`);
-    const result = compareFiles(firstConfig, secondConfig);
-    console.log(result);
-  });
+    .version('0.0.3')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f --format <type>', 'output format')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => {
+        console.log(`${firstConfig} ${secondConfig}`);
+        const result = compareFiles(firstConfig, secondConfig);
+        console.log(result);
+    });
 program.parse(process.argv);
