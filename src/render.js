ea96ef43c2ec0caec28d1369358d2bebb8d85171
Heizo <heizoinside@gmail.com>
2020-03-22 13:23:25 +0300

    add 4 space indent

--- src/render.js
+++ src/render.js
@@ -4,21 +4,21 @@ import yaml from 'js-yaml';
 import ini from 'ini';
 
 const mapping = {
-  json: JSON.parse,
-  yaml: yaml.safeLoad,
-  ini: ini.parse,
+    json: JSON.parse,
+    yaml: yaml.safeLoad,
+    ini: ini.parse,
 };
 
 const getType = (filepath) => path.extname(filepath).slice(1).toLowerCase().trim();
 
 const getFileContent = (filepath) => {
-  const currentDir = process.cwd();
-  const absolutePath = path.resolve(currentDir, filepath);
-  return fs.readFileSync(absolutePath, 'utf-8');
+    const currentDir = process.cwd();
+    const absolutePath = path.resolve(currentDir, filepath);
+    return fs.readFileSync(absolutePath, 'utf-8');
 };
 
 export default (filepath) => {
-  const currentType = getType(filepath);
-  const currentContent = getFileContent(filepath);
-  return mapping[currentType](currentContent);
+    const currentType = getType(filepath);
+    const currentContent = getFileContent(filepath);
+    return mapping[currentType](currentContent);
 };
