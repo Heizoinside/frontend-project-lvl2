69f9797154331ded6ace7d06058b00f779237411
Heizo <heizoinside@gmail.com>
2020-03-22 14:23:16 +0300

    divide index on modules

--- src/index.js
+++ src/index.js
@@ -1,37 +1,6 @@
-import _ from 'lodash';
-import parse from './render';
+import parse from './parse';
 import buildAst from './astBuilder';
-
-const makeTab = (tabCount) => ' '.repeat(tabCount);
-
-const stringify = (key, value, tab) => {
-    if (!_.isObject(value)) {
-        return `${makeTab(tab)}${key}: ${value}`;
-    }
-    const innerContent = Object.keys(value).map((el) => (
-        stringify(el, value[el], tab + 5)
-    ));
-    return `${makeTab(tab)}${key}: {\n${innerContent}\n${makeTab(tab + 2)}}`;
-};
-
-const operations = {
-    unchanged: (node, tab) => stringify(`  ${node.name}`, node.before, tab),
-    changed: (node, tab) => {
-        const conentBefore = stringify(`- ${node.name}`, node.before, tab);
-        const contentAfter = stringify(`+ ${node.name}`, node.after, tab);
-        return `${conentBefore}\n${contentAfter}`;
-    },
-    added: (node, tab) => stringify(`+ ${node.name}`, node.after, tab),
-    deleted: (node, tab) => stringify(`- ${node.name}`, node.before, tab),
-    nested: (node, tab, func) => {
-        const childsContent = node.children.map((el) => operations[el.type](el, tab + 3, func)).join('\n');
-        return `${makeTab(tab + 2)}${node.name}: {\n${childsContent}\n${makeTab(tab + 2)}}`;
-    },
-};
-
-const render = (ast) => (
-    ast.map((el) => operations[el.type](el, 0, render)).join('\n')
-);
+import render from './render';
 
 export default (filepath1, filepath2) => {
     const fileBefore = parse(filepath1);
--- src/render.js
+++ src/render.js
@@ -1,24 +1,34 @@
-import * as fs from 'fs';
-import path from 'path';
-import yaml from 'js-yaml';
-import ini from 'ini';
+import _ from 'lodash';
 
-const mapping = {
-    json: JSON.parse,
-    yaml: yaml.safeLoad,
-    ini: ini.parse,
+const makeTab = (tabCount) => ' '.repeat(tabCount);
+
+const stringify = (key, value, tab) => {
+    if (!_.isObject(value)) {
+        return `${makeTab(tab)}${key}: ${value}`;
+    }
+    const innerContent = Object.keys(value).map((el) => (
+        stringify(el, value[el], tab + 5)
+    ));
+    return `${makeTab(tab)}${key}: {\n${innerContent}\n${makeTab(tab + 2)}}`;
 };
 
-const getType = (filepath) => path.extname(filepath).slice(1).toLowerCase().trim();
-
-const getFileContent = (filepath) => {
-    const currentDir = process.cwd();
-    const absolutePath = path.resolve(currentDir, filepath);
-    return fs.readFileSync(absolutePath, 'utf-8');
+const operations = {
+    unchanged: (node, tab) => stringify(`  ${node.name}`, node.before, tab),
+    changed: (node, tab) => {
+        const conentBefore = stringify(`- ${node.name}`, node.before, tab);
+        const contentAfter = stringify(`+ ${node.name}`, node.after, tab);
+        return `${conentBefore}\n${contentAfter}`;
+    },
+    added: (node, tab) => stringify(`+ ${node.name}`, node.after, tab),
+    deleted: (node, tab) => stringify(`- ${node.name}`, node.before, tab),
+    nested: (node, tab, func) => {
+        const childsContent = node.children.map((el) => operations[el.type](el, tab + 3, func)).join('\n');
+        return `${makeTab(tab + 2)}${node.name}: {\n${childsContent}\n${makeTab(tab + 2)}}`;
+    },
 };
 
-export default (filepath) => {
-    const currentType = getType(filepath);
-    const currentContent = getFileContent(filepath);
-    return mapping[currentType](currentContent);
-};
+const render = (ast) => (
+    ast.map((el) => operations[el.type](el, 0, render)).join('\n')
+);
+
+export default render;
