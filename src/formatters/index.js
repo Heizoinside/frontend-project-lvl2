a1ba6efca8a4a99bcac1e015725161fae979142b
Heizoinside <34223405+Heizoinside@users.noreply.github.com>
2020-03-27 18:00:45 +0300

    Update index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,11 @@
-import treeRender from './render-tree';
-import plainRender from './render-plain';
-import jsonRender from './render-json';
+import renderTree from './render-tree';
+import renderPlain from './render-plain';
+import renderJson from './render-json';
 
 const extensions = {
-    tree: (ast) => treeRender(ast),
-    plain: (ast) => plainRender(ast),
-    json: (ast) => jsonRender(ast),
+    tree: (ast) => renderTree(ast),
+    plain: (ast) => renderPlain(ast),
+    json: (ast) => renderJson(ast),
 };
 
 export default (ext, ast) => extensions[ext](ast);
