fc9b977e42e595aefa2643f80f6d521d4b87e074
Heizoinside <34223405+Heizoinside@users.noreply.github.com>
2020-03-27 11:02:36 +0300

    refactoring

--- src/formatters/render-plain.js
+++ src/formatters/render-plain.js
@@ -3,21 +3,21 @@ import _ from 'lodash';
 const getValueContent = (value) => (_.isObject(value) ? ['[complex value]'] : value);
 const plainOperations = {
     unchanged: () => null,
-    changed: (node, path) => `Property ${path}${node.name} was changed from ${node.before} to ${getValueContent(node.after)}`,
-    added: (node, path) => `Property ${path}${node.name} was added with value ${getValueContent(node.after)}`,
-    deleted: (node, path) => `Property ${path}${node.name} was deleted`,
-    nested: (node, path, func) => {
-        const result = node.children.map((el) => plainOperations[el.type](el, `${path}${node.name}.`, func));
+    changed: (node, filepath) => `Property ${filepath}${node.name} was changed from ${node.before} to ${getValueContent(node.after)}`,
+    added: (node, filepath) => `Property ${filepath}${node.name} was added with value ${getValueContent(node.after)}`,
+    deleted: (node, filepath) => `Property ${filepath}${node.name} was deleted`,
+    nested: (node, filepath, func) => {
+        const result = node.children.map((el) => plainOperations[el.type](el, `${filepath}${node.name}.`, func));
         return result;
     },
 };
 
 const plainRender = (ast) => {
-    const renderedAst = ast.map((node) => {
+    const processedAst = ast.map((node) => {
         const result = plainOperations[node.type](node, '', plainRender);
         return result;
     });
-    return _.flattenDeep(renderedAst)
+    return _.flattenDeep(processedAst)
         .filter((el) => el !== null)
         .join('\n');
 };
