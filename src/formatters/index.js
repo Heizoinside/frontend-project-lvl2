c1308f9b2baf7a58f5b8d30876502c6394c908eb
Heizoinside <34223405+Heizoinside@users.noreply.github.com>
2020-03-27 11:56:41 +0300

    refactoring

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,10 +2,10 @@ import treeRender from './render-tree';
 import plainRender from './render-plain';
 import jsonRender from './render-json';
 
-const formats = {
+const extensions = {
     tree: (ast) => treeRender(ast),
     plain: (ast) => plainRender(ast),
     json: (ast) => jsonRender(ast),
 };
 
-export default (format, ast) => formats[format](ast);
+export default (ext, ast) => extensions[ext](ast);
