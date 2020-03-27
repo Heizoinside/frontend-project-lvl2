115753fa8121dcae5f7995e572d17b3feeddd91d
Heizoinside <34223405+Heizoinside@users.noreply.github.com>
2020-03-27 15:36:01 +0300

    refactoring

--- src/formatters/render-plain.js
+++ src/formatters/render-plain.js
@@ -3,8 +3,8 @@ import _ from 'lodash';
 const getValueContent = (value) => (_.isObject(value) ? ['[complex value]'] : value);
 const plainOperations = {
     unchanged: () => null,
-    changed: (node, filepath) => `Property ${filepath}${node.name} was changed from ${node.before} to ${getValueContent(node.after)}`,
-    added: (node, filepath) => `Property ${filepath}${node.name} was added with value ${getValueContent(node.after)}`,
+    changed: (node, filepath) => `Property ${filepath}${node.name} was changed from ${node.valueBefore} to ${getValueContent(node.valueAfter)}`,
+    added: (node, filepath) => `Property ${filepath}${node.name} was added with value ${getValueContent(node.valueAfter)}`,
     deleted: (node, filepath) => `Property ${filepath}${node.name} was deleted`,
     nested: (node, filepath, func) => {
         const result = node.children.map((el) => plainOperations[el.type](el, `${filepath}${node.name}.`, func));
