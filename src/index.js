70e3ec93f2a5df98611593afad420820fcc8ae1a
Heizo <heizoinside@gmail.com>
2020-03-28 18:17:15 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import parse from './parse';
 import buildAst from './ast-builder';
 import render from './formatters';
 
-export default (extension, filepathBefore, filepathAfter) => {
+export default (outputFormat = 'tree', filepathBefore, filepathAfter) => {
     const contentBefore = parse(filepathBefore);
     const contentAfter = parse(filepathAfter);
     const ast = buildAst(contentBefore, contentAfter);
-    return render(extension, ast);
+    return render(outputFormat, ast);
 };
